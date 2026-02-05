c1ad3a3701363c92ae2f9006ac55b498610f0745
Akash Srivastava <akash.1805.srivastava@gmail.com>
2026-02-06 00:05:21 +0530

    Refactor Docker push function to include user parameter

--- vars/pushing_to_docker_hub.groovy
+++ vars/pushing_to_docker_hub.groovy
@@ -1,12 +1,17 @@
-def call(String project_name , String image_tag){
-  echo "Pushing the ${project_name} image to docker hub"
+def call(String docker_user, String project_name, String image_tag) {
+  echo "Pushing the ${project_name} image to Docker Hub"
+
   withCredentials([
-    usernamePassword('credentialsId':"DockerHubCred",
-    passwordVariable:"dockerHubPass",
-    usernameVariable:"dockerHubUser")]){
-      sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-      sh "docker image tag ${project_name}:${image_tag} ${env.dockerHubUser}/${project_name}:${image_tag}"
-      sh "docker push ${env.dockerHubUser}/${project_name}:${image_tag}"
-    }
-  echo "${project_name} image is successfully pushed to docker hub"
+    usernamePassword(
+      credentialsId: 'DockerHubCred',
+      usernameVariable: 'dockerHubUser',
+      passwordVariable: 'dockerHubPass'
+    )
+  ]) {
+    sh 'echo $dockerHubPass | docker login -u $dockerHubUser --password-stdin'
+
+    sh "docker push ${docker_user}/${project_name}:${image_tag}"
+  }
+
+  echo "${project_name} image successfully pushed to Docker Hub"
 }
